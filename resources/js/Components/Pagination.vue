e97d7a795d6e58ffc0248c333df8822ee6f3afc5
Donovann Liza <dliza@worldcob.com>
2025-02-10 20:01:27 -0500

    pagination

---
+++ resources/js/Components/Pagination.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { Link } from '@inertiajs/vue3';
+defineProps({
+    links: {
+        type: Array,
+        required: true
+    }
+})
+
+const makeLabel = (label) => {
+    if (label.includes("Previous")) {
+        return '<'
+    } else if (label.includes("Next")) {
+        return '>'
+    } else {
+        return label
+    }
+}
+</script>
+<template>
+    <div v-if="links.length > 3" class="flex items-center justify-end mt-4">
+        <div class="flex items-center space-x-1">
+            <div v-for="(link, i) in links" :key="i">
+                <component 
+                    :is="link.url && !link.active ? Link : 'span'"
+                    :href="link.url && !link.active ? link.url : null"
+                    class="px-4 py-2 border rounded-md"
+                    :class="{
+                        'bg-blue-500 text-white': link.active,
+                        'bg-white text-gray-700 hover:bg-gray-50': !link.active
+                    }"
+                    v-html="makeLabel(link.label)"
+                />
+            </div>
+        </div>
+    </div>
+</template>
