38e4914c9b17b3e7e34c1b19b718eec54df78aa2
Donovann Liza <dliza@worldcob.com>
2025-02-10 22:13:54 -0500

    Datatable icon

--- resources/js/Components/DataTable.vue
+++ resources/js/Components/DataTable.vue
@@ -42,7 +42,7 @@ watch(searchQuery, (value) => {
             <div class="relative">
                 <input type="search" v-model="searchQuery" :placeholder="searchPlaceholder"
                     class="w-64 pl-10 pr-4 py-2 border rounded-md focus:ring-2 focus:ring-blue-500" />
-                <SearchIcon class="absolute w-5 h-5 text-gray-400 left-3 top-2.5" />
+                <MagnifyingGlassIcon class="absolute w-5 h-5 text-gray-400 left-3 top-2.5" />
             </div>
         </div>
 
