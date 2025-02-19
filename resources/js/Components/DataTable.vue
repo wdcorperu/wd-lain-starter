073083fb224829f23fc58fc7a9b438d5b5df7cb9
Donovann Liza <dliza@worldcob.com>
2025-02-19 17:56:16 -0500

    fix(datatable) correct mode dark

--- resources/js/Components/DataTable.vue
+++ resources/js/Components/DataTable.vue
@@ -4,6 +4,8 @@ import { router } from '@inertiajs/vue3';
 import { MagnifyingGlassIcon } from '@heroicons/vue/24/outline';
 import debounce from 'debounce';
 import Pagination from '@/Components/Pagination.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+import { PlusIcon } from '@heroicons/vue/24/outline';
 
 const props = defineProps({
     searchPlaceholder: {
@@ -22,6 +24,10 @@ const props = defineProps({
         type: String,
         required: true,
     },
+    btnCreate: {
+        type: Boolean,
+        default: false,
+    }
 });
 
 const searchQuery = ref('');
@@ -36,35 +42,40 @@ watch(searchQuery, (value) => {
 </script>
 <template>
     <div>
-        <div class="bg-white shadow rounded-lg overflow-hidden p-4">
-            <!-- Search -->
-            <div class="flex items-center justify-start mb-4">
+        <div class="bg-wd-l-4 dark:bg-wd-d-6 shadow rounded-lg overflow-hidden p-4">
+            <!-- Search - Create -->
+            <div class="flex items-center justify-between mb-4">
                 <div class="relative">
                     <input type="search" v-model="searchQuery" :placeholder="searchPlaceholder"
-                        class="w-64 pl-10 pr-4 py-2 border border-gray-300 placeholder:text-gray-400 rounded-md focus:ring-1 focus:ring-blue-200" />
-                    <MagnifyingGlassIcon class="absolute w-5 h-5 text-gray-400 left-3 top-2.5" />
+                        class="w-64 pl-10 py-2 bg-wd-l-4 dark:bg-wd-d-7 border-wd-l-placeholder dark:border-wd-d-placeholder placeholder:text-wd-l-placeholder dark:placeholder:text-wd-d-placeholder text-wd-l-text dark:text-wd-d-text rounded-md focus:ring-1 focus:ring-wd-1" />
+                    <MagnifyingGlassIcon class="absolute w-5 h-5 text-wd-l-placeholder dark:text-wd-d-placeholder left-3 top-2.5" />
                 </div>
+
+                <PrimaryButton v-if="btnCreate" type="button" class="bg-nc-primary" @click="$emit('open-modal', 'create')">
+                    {{ $t('admin.datatable.create') }}
+                    <PlusIcon class="w-4 h-4 ml-3" />
+                </PrimaryButton>
             </div>
             <!-- Table -->
             <div class="overflow-x-auto">
-                <table class="min-w-full divide-y divide-gray-200">
-                    <thead class="bg-gray-50">
+                <table class="min-w-full">
+                    <thead class="bg-wd-l-5 dark:bg-wd-d-7">
                         <tr>
                             <th v-for="column in columns" :key="column.key" :class="[
-                                'px-6 py-3 text-left text-xs font-semibold text-gray-500 uppercase tracking-wider',
+                                'px-6 py-3 text-left text-xs font-bold text-wd-l-text dark:text-wd-d-text uppercase tracking-wider',
                                 column.class
                             ]">
                                 {{ column.label }}
                             </th>
                             <th v-if="$slots.actions"
-                                class="px-6 py-3 text-right text-xs font-semibold text-gray-500 uppercase tracking-wider">
-                                Acciones
+                                class="px-6 py-3 text-right text-xs font-bold text-wd-l-text dark:text-wd-d-text uppercase tracking-wider">
+                                {{ $t('admin.datatable.actions') }}
                             </th>
                         </tr>
                     </thead>
-                    <tbody class="bg-white divide-y divide-gray-200">
+                    <tbody class="bg-wd-l-4 dark:bg-wd-d-6 divide-y divide-wd-l-5 dark:divide-wd-d-7">
                         <tr v-for="item in data.data" :key="item.id">
-                            <td v-for="column in columns" :key="column.key" class="px-6 py-4 whitespace-nowrap text-sm">
+                            <td v-for="column in columns" :key="column.key" class="px-6 py-3 whitespace-nowrap text-sm font-extralight text-wd-l-text dark:text-wd-d-text">
                                 <!-- Slot personalizado para la columna -->
                                 <slot :name="column.key" :item="item" :value="item[column.key]">
                                     {{ item[column.key] }}
@@ -72,7 +83,7 @@ watch(searchQuery, (value) => {
                             </td>
                             <!-- Slot para acciones -->
                             <td v-if="$slots.actions"
-                                class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                class="px-6 py-3 whitespace-nowrap text-right text-sm font-medium">
                                 <slot name="actions" :item="item"></slot>
                             </td>
                         </tr>
@@ -80,7 +91,7 @@ watch(searchQuery, (value) => {
                         <tr v-if="data.data.length === 0">
                             <td :colspan="columns.length + ($slots.actions ? 1 : 0)"
                                 class="px-6 py-4 text-center text-gray-500">
-                                No se encontraron resultados
+                                {{ $t('admin.datatable.not_results') }}
                             </td>
                         </tr>
                     </tbody>
--- resources/js/Components/Pagination.vue
+++ resources/js/Components/Pagination.vue
@@ -1,36 +1,30 @@
 <script setup>
 import { Link } from '@inertiajs/vue3';
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/vue/24/outline';
+
 defineProps({
     links: {
         type: Array,
         required: true
     }
 })
-
-const makeLabel = (label) => {
-    if (label.includes("Previous")) {
-        return '<'
-    } else if (label.includes("Next")) {
-        return '>'
-    } else {
-        return label
-    }
-}
 </script>
 <template>
     <div v-if="links.length > 3" class="flex items-center justify-end mt-4">
-        <div class="flex items-center space-x-1">
-            <div v-for="(link, i) in links" :key="i">
+        <div class="flex items-center space-x-2">
+            <div v-for="(link, i) in links" :key="i" class="w-10 h-10">
                 <component 
                     :is="link.url && !link.active ? Link : 'span'"
                     :href="link.url && !link.active ? link.url : null"
-                    class="px-4 py-2 rounded-md shadow-sm"
-                    :class="{
-                        'bg-gray-800 text-white': link.active,
-                        'bg-white text-gray-700 hover:bg-gray-50': !link.active
-                    }"
-                    v-html="makeLabel(link.label)"
-                />
+                    :class="[
+                        link.active ? 'bg-wd-d-6 text-white' : 'bg-wd-l-4 dark:bg-wd-d-6/40 text-wd-l-label dark:text-wd-d-label hover:bg-gray-50',
+                        'w-full h-full flex items-center justify-center rounded-md shadow-sm'
+                    ]"
+                >
+                    <ChevronLeftIcon v-if="link.label.includes('Previous')" class="h-5 w-5" />
+                    <ChevronRightIcon v-else-if="link.label.includes('Next')" class="h-5 w-5" />
+                    <span v-else v-html="link.label"></span>
+                </component>
             </div>
         </div>
     </div>
